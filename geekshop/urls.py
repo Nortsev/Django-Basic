d6a68b8e5ab7f5ff5bd1128256fc1eb85adae355
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2021-03-19 22:25:53 +0400

    Add maimapp/urls.py

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -18,11 +18,14 @@ from django.urls import path
 from mainapp import views as mainapp_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp_views.index, name='index'),
-    path('products/', mainapp_views.products, name='products'),
+    path("products/", include("mainapp.urls", namespace="products")),
     path('contacts/', mainapp_views.contacts, name='contacts'),
 ]
 
